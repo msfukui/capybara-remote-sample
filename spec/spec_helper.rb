5954fc14d4bd92612a8cf3dfed45256252321b2c
msfukui <msfukui@gmail.com>
2015-04-28 22:25:20 +0900

    ブラウザ、リモートアドレス、ポート番号を環境変数から取れる様に修正。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,23 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
+EXECUTABLE_BROWSERS = ['ie', 'internet_explorer', 'chrome', 'firefox']
+webbrowser = ENV['EXECUTE_BROWSER'] || 'firefox'
+webbrowser = 'firefox' unless EXECUTABLE_BROWSERS.include?(webbrowser)
+
+host = ENV['EXECUTE_HOST'] || '192.168.11.3'
+port = ENV['EXECUTE_PORT'] || '4444'
+
 RSpec.configure do |config|
   config.include Capybara::DSL
 end
-
-host = '192.168.11.3'
-port = '4444'
 
 Capybara.register_driver :remote_windows do |app|
   Capybara::Selenium::Driver.new(
     app,
     browser: :remote,
     url: "http://#{host}:#{port}/wd/hub/",
-    desired_capabilities: :firefox
+    desired_capabilities: webbrowser.to_sym
   )
 end
 
