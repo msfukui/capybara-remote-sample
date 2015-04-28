f7dc0f4c2fcc9d56cc0204b01c1d5b9a8e61f825
msfukui <msfukui@gmail.com>
2015-04-28 22:29:35 +0900

    タイムアウト値を設定して hidden を拾わない様に定義を追加。

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,16 @@
 require 'capybara/rspec'
 require 'selenium-webdriver'
 
+RSpec.configure do |config|
+  config.include Capybara::DSL
+end
+
 EXECUTABLE_BROWSERS = ['ie', 'internet_explorer', 'chrome', 'firefox']
 webbrowser = ENV['EXECUTE_BROWSER'] || 'firefox'
 webbrowser = 'firefox' unless EXECUTABLE_BROWSERS.include?(webbrowser)
 
 host = ENV['EXECUTE_HOST'] || '192.168.11.3'
 port = ENV['EXECUTE_PORT'] || '4444'
-
-RSpec.configure do |config|
-  config.include Capybara::DSL
-end
 
 Capybara.register_driver :remote_windows do |app|
   Capybara::Selenium::Driver.new(
@@ -22,5 +22,9 @@ Capybara.register_driver :remote_windows do |app|
 end
 
 Capybara.default_driver = :remote_windows
+
+Capybara.ignore_hidden_elements = true
+
+Capybara.default_wait_time = 15
 
 Capybara.app_host = 'http://www.biglobe.ne.jp'
